32e0b3f024977383630401202a32bf50cee2c699
Carlos Rony <carlosrecla03@gmail.com>
2021-11-03 17:23:35 -0300

    fina da tarefa

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, peso, imc;
+
+            Console.Write("Altura (m)..:");
+            altura = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Peso (kg)...:");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine($"IMC: {imc:N1} kg/m²");
+
+        }
+    }
+}
